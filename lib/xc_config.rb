8ebcc5c042b02fd7d8c7cfc53882329f3fcabdcb
Hiroaki Muronaka <hiroaki.muronaka@plus-add.com>
2015-08-26 09:00:19 +0900

    add xc_config

---
+++ lib/xc_config.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+require 'yaml'
+
+CONFIG_PATH=File.expand_path("~/.xc_config", __FILE__)
+
+def load_config
+  load_config_with_path(CONFIG_PATH)
+end
+
+def load_config_with_path(yaml_path)
+  config = {}
+  begin
+    str = File.read(yaml_path)
+    config = YAML.load(str)
+  rescue => e
+  end
+
+  if config[:search_paths].nil?
+    config[:search_paths] = ["."]
+  end
+  if config[:search_depth].nil?
+    config[:search_depth] = 4
+  end
+  if config[:exclude_paths].nil?
+    config[:exclude_paths] = [".git", "Pods"]
+  end
+  if config[:history_num].nil?
+    config[:history_num] = 12
+  end
+
+  config
+end
